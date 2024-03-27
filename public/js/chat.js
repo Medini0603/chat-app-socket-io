eec3ce712a35e4b8d53d6cb613936ebe58bb6ba1
Medini <medu0603@gmail.com>
2024-03-27 12:38:33 +0530

    a simple count app to understand socket io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,22 @@
-io()
+//to send and recieve events with server
+const socket=io()
+
+//Count app
+// server stores a single number called count and it will show the count to all connected clients
+//client will also have abutton to click to increment count and it will be updates on all clients
+
+//rn index.js --server
+//chat.js --client
+
+//to recieve event from server
+//same name of function as in server
+//acess the variable sent by server using param of callback function
+socket.on('countUpdated',(count)=>{
+    console.log("Your count has been updated",count)
+})
+
+document.getElementById('increment').addEventListener('click',()=>{
+    console.log("Clicked")
+    //when clicked increment it and emit it to server
+    socket.emit('increment')
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path=require("path")
 const http=require("http")
 const express=require("express")
 const socketio=require('socket.io')
+const { emit } = require("process")
 
 const app=express()
 const server=http.createServer(app)
@@ -12,9 +13,29 @@ const port=process.env.PORT||3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
+let count=0
+//this function runs once for each client
+//i.e. if 5 clients then 5 times
 
-io.on('connection',()=>{
+//count app 2 function
+//server emit-> client recieve - countUpdated
+//client emit-> server recieve - increment
+
+
+io.on('connection',(socket)=>{
     console.log('New WebSocket connection')
+    //to send event from server along with count
+    socket.emit('countUpdated',count)
+    //listen to increment from clients the name is the same function name as in client
+    socket.on('increment',()=>{
+        count++
+        //it emits to only one client i.e the one it is updating
+        // socket.emit('countUpdated',count)
+
+        //if we have 5 browser windows with localhost:3000 and one clicks button output is shown for all console of clients
+        //this emits on all clients i.e TO EVERY SINGLE CONNECTION!!!
+        io.emit('countUpdated',count)
+    })
 })
 
 server.listen(port,()=>{
