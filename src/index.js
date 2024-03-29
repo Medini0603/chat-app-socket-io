b4edc692c9332fa403d4cb675319eff10aeaf5c9
Medini <medu0603@gmail.com>
2024-03-29 11:35:54 +0530

    message from clients added

--- src/index.js
+++ src/index.js
@@ -13,28 +13,15 @@ const port=process.env.PORT||3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
-let count=0
-//this function runs once for each client
-//i.e. if 5 clients then 5 times
-
-//count app 2 function
-//server emit-> client recieve - countUpdated
-//client emit-> server recieve - increment
 
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection')
-    //to send event from server along with count
-    socket.emit('countUpdated',count)
-    //listen to increment from clients the name is the same function name as in client
-    socket.on('increment',()=>{
-        count++
-        //it emits to only one client i.e the one it is updating
-        // socket.emit('countUpdated',count)
-
-        //if we have 5 browser windows with localhost:3000 and one clicks button output is shown for all console of clients
-        //this emits on all clients i.e TO EVERY SINGLE CONNECTION!!!
-        io.emit('countUpdated',count)
+    //emits to single client
+    socket.emit('message','WELCOME!')
+    socket.on('sendMessage',(message)=>{
+        //emits to all client
+        io.emit("message",message)
     })
 })
 
